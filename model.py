70d66f339a328e1bd35db0b4114fd4d84be908b2
Your Name <you@example.com>
2020-02-21 14:08:31 -0800

    movie_load() part2

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ class Movie(db.Model):
     __tablename__ = "movies"
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(64), nullable=False)
+    title = db.Colpassumn(db.String(64), nullable=False)
     released_at = db.Column(db.DateTime, nullable=False)
     imdb_url = db.Column(db.String(100), nullable=False)
 
